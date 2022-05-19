4a5d2e3501445a3d6fec6c6a930a3c8eb086f6c9
Stephen Brooks <codedev1980@gmail.com>
2022-05-19 13:47:13 -0500

    added facebook nav icon

--- controllers/updateUser.js
+++ controllers/updateUser.js
@@ -2,18 +2,10 @@ const userModel = require('../models/User')
 
 module.exports = async (req, res) => {
     try {
-        const { todo } = req.body
-        const update = await userModel.findOneAndUpdate({_id: req.params.id}, {todo})
-        if (!update) {
-            return res.status(400).json({
-                success: false,
-                message: 'Not successfully updated'
-            })
-        }
-        return res.status(200).json({
-            success: true,
-            message: 'Profile successfully updated'
-        })
+        await userModel.findByIdAndUpdate(id,{
+            phone: 'Update phone',
+            email: 'Updated email'
+        }, res.render('employee'))
     } catch (error) {
         return res.status(400).json({
             success: false,
